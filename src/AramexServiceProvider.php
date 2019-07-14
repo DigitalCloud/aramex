5d1d502040f7c43a4fd2fd4ef97ae9b6f64dfe2a
Yassir Awad <yassir.awad@dce.sa>
2019-07-14 12:29:43 +0300

    first commit

---
+++ src/AramexServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aramex;
+
+use Illuminate\Support\ServiceProvider;
+
+class AramexServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
